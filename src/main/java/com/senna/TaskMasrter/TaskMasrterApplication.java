1e4e0df2e9dbfb34e64d0ac5fd7e09add182d643
felipesn <felipesn@ciandt.com>
2024-09-30 07:34:45 -0300

    Meu primeiro commit do projeto TaskMaster

---
+++ src/main/java/com/senna/TaskMasrter/TaskMasrterApplication.java
@@ -0,0 +1,13 @@
+package com.senna.TaskMasrter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMasrterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMasrterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senna/TaskMasrter/TaskMasrterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senna.TaskMasrter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMasrterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
